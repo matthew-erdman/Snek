9a5d6c04fe393de731c157f6bebe0214dde7a5d4
matthew-erdman <matthew.erdman.23@bishops.com>
2020-12-14 15:10:07 -0800

    Frame timing test

--- snek.js
+++ snek.js
@@ -1,10 +1,8 @@
 function play() {
-	if (playing % 2 != 0) {
-	  frames += 1;
-	  if (frames % 30 == 0) {
-	      snek.move();
-	      apple.checkCollision();
-	  }
+	if (playing % 2 == 1) {
+	  snek.move();
+    console.log(snek.pos)
+	  apple.checkCollision();
 	  snek.checkBounds();
 	  drawAll();
 	}
@@ -13,8 +11,10 @@ function play() {
 
 document.addEventListener("keydown", keypress);
 ctx = setUpContext();
-snek = new Snek(160, 160, ctx);
+snek = new Snek(canvas.width / 2 - (canvas.width / 2) % 40, canvas.height / 2 - (canvas.height / 2) % 40, ctx);
+console.log("New snek spawned at " + snek.pos)
 apple = new Apple();
+apple.spawn();
 frames = 0;
 playing = 1;
 window.requestAnimationFrame(play);
--- snekClasses.js
+++ snekClasses.js
@@ -2,7 +2,8 @@ class Snek {
     constructor(x, y) {
       this.pos = [x, y];
       this.color = "#00c000";
-      this.direction = 0
+      this.dir = 0;
+      this.len = 1;
     }
     get x() {return this.pos[0];}
     set x(newX) {this.pos[0] = newX;}
@@ -11,16 +12,16 @@ class Snek {
     set y(newY) {this.pos[1] = newY;}
 
     move() {
-      if (this.direction == 0) {
+      if (this.dir == 0) {
         this.y -= 40;
       }
-      else if (this.direction == 1) {
+      else if (this.dir == 1) {
         this.x += 40;
       }
-      else if (this.direction == 2) {
+      else if (this.dir == 2) {
         this.y += 40;
       }
-      else if (this.direction == 3) {
+      else if (this.dir == 3) {
         this.x -= 40;
       }
     }
@@ -28,34 +29,38 @@ class Snek {
     checkBounds() {
       if (this.x >= canvas.width) {
         this.pos = [160, 160];
-        this.direction = 0;
-        console.log("X bound + respawn")
-        playing = false;
+        this.dir = 0;
+        console.log("X bound + (right) collision")
+        snek.len = 1;
+        pause();
       }
       else if (this.x < 0) {
         this.pos = [160, 160];
-        this.direction = 0;
-        console.log("X bound - respawn")
-        playing = false;
+        this.dir = 0;
+        console.log("X bound - (left) collision")
+        pause();
+        snek.len = 1;
       }
       else if (this.y >= canvas.height) {
         this.pos = [160, 160];
-        this.direction = 0;
-        console.log("Y bound + respawn")
-        playing = false;
+        this.dir = 0;
+        console.log("Y bound + (down) collision")
+        pause();
+        snek.len = 1;
       }
       else if (this.y < 0) {
         this.pos = [160, 160];
-        this.direction = 0;
-        console.log("Y bound - respawn")
-        playing += 1;
+        this.dir = 0;
+        console.log("Y bound - (up) collision")
+        pause();
+        snek.len = 1;
       }
     }
 }
 
 class Apple {
   constructor() {
-    this.pos = [320, 320];
+    this.pos = [-40, -40];
     this.color = "#ff0000";
   }
 
@@ -66,9 +71,9 @@ class Apple {
   set y(newY) {this.pos[1] = newY;}
 
   checkCollision() {
-    console.log(snek.pos, this.pos)
     if (snek.x == this.x && snek.y == this.y) {
-      console.log("apple")
+      snek.len += 1;
+      console.log("Apple collected at %s len: %d", this.pos, snek.len);
       this.spawn();
     }
   }
@@ -76,16 +81,44 @@ class Apple {
   spawn() {
     this.x = Math.floor(Math.random() * (canvas.width / 40)) * 40;
     this.y = Math.floor(Math.random() * (canvas.height / 40)) * 40;
+    console.log("Apple spawned at " + this.pos);
   }
 }
 
+function pause() {
+  if (playing % 2 == 1) {
+    ctx.font = "30px Arial";
+    ctx.strokeText("Paused", canvas.width / 2, canvas.height / 2);
+    console.log("Execution paused");
+  }
+  else {
+    console.log("Execution unpaused");
+  }
+  playing += 1;
+}
+
 function drawAll() {
+  // Clear screen for new frame
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draw apple
+  ctx.fillStyle = apple.color;
+  ctx.fillRect(apple.x, apple.y, 40, 40);
+  ctx.stroke();
+
+  // Draw snek
   ctx.fillStyle = snek.color;
   ctx.fillRect(snek.x, snek.y, 40, 40);
   ctx.stroke();
-  ctx.fillStyle = apple.color;
-  ctx.fillRect(apple.x, apple.y, 40, 40);
+
+  // Draw score box
+  ctx.strokeStyle = snek.color;
+  ctx.rect(0, 0, 110, 30);
+  ctx.stroke();
+
+  //Draw score
+  ctx.font = "20px Cursive";
+  ctx.fillText("Score: " + snek.len, 5, 20);
   ctx.stroke();
 }
 
@@ -104,19 +137,20 @@ function setUpContext() {
 
 function keypress(event) {
   keyStr = event.key;
-  if (keyStr == 'w') {
-    snek.direction = 0;
+  if ((playing % 2 == 0 && ["w", "a", "s", "d"].includes(keyStr))|| keyStr == 'Escape') {
+    console.log("Manual pause triggered");
+    pause();
   }
-  if (keyStr == 'd') {
-    snek.direction = 1;
+  else if (keyStr == 'w') {
+    snek.dir = 0;
   }
-  if (keyStr == 's') {
-    snek.direction = 2;
+  else if (keyStr == 'd') {
+    snek.dir = 1;
   }
-  if (keyStr == 'a') {
-    snek.direction = 3;
+  else if (keyStr == 's') {
+    snek.dir = 2;
   }
-  if (keyStr == 'Escape') {
-    playing += 1;
+  else if (keyStr == 'a') {
+    snek.dir = 3;
   }
 }
