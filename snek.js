54f7189ab58375e81e2bf4085133e1e7835b2f58
matthew-erdman <matthew.erdman.23@bishops.com>
2020-12-18 03:31:29 -0800

    Functional game on Windows

--- snek.js
+++ snek.js
@@ -1,25 +1,60 @@
 function play() {
-  if (playing % 2 == 1) {
-	  frames += 1;
-	  if (frames % 30 == 0) {
-	      snek.move();
-        console.log(snek.pos)
-	      apple.checkCollision();
-	  }
-	  snek.checkBounds();
-	  drawAll();
+/*
+  Purpose: Serves as the main drawing loop
+  Inputs: None
+  Returns: None, recurses infinitely
+*/
+
+  // Only render 1 of every every 20 frames
+  if (playing && frames % 20 == 0) {
+      snek.move();                            // Move snek
+      apple.checkEaten();                     // Is snek on apple?
+      snek.checkCollision();                  // Did snek die?
+      if (snek.tail.length + 1 > highScore) {
+        highScore = snek.tail.length + 1;     // Do we have to update highscore?
+      }
+      drawAll();                              // Draw frame
+
+      // FPS counter, logs every 10 secs
+      if (frames % 200 == 0) {
+        now = new Date();
+        msecs = now.getTime() - start.getTime();
+        console.log("fps:", Math.round((frames / msecs) * 1000));
+      }
 	}
+
+  frames += 1;
   window.requestAnimationFrame(play);
+  //NOTE: 144 fps on Windows with 144 Hz main display and ? fps on Mac with 60 Hz display
 }
 
+// ---Init---
+// Set up keyboard event listener
 document.addEventListener("keydown", keypress);
-ctx = setUpContext();
-snek = new Snek(canvas.width / 2 - (canvas.width / 2) % 40, canvas.height / 2 - (canvas.height / 2) % 40, ctx);
-console.log("New snek spawned at " + snek.pos)
-apple = new Apple();
-apple.spawn();
-playing = 1;
+
+// Set up canvas context
+var ctx = setUpContext();
+
+// FPS Counter vars
 var frames = 0;
 var start = new Date();
 var now = new Date();
+
+// Global game vars
+var playing = true;
+var appleEaten = false;
+var highScore = 0;
+
+// Object instantiations
+var snek = new Snek(ctx);
+var apple = new Apple();
+
+// Logging
+console.log("Window is %d by %d", window.innerWidth, window.innerHeight);
+console.log("Canvas is %d by %d", canvas.width, canvas.height);
+console.log("New snek spawned at " + snek.pos)
+
+apple.spawn();
+
+// Request first frame and enter game loop
 window.requestAnimationFrame(play);
--- snekClasses.js
+++ snekClasses.js
@@ -1,10 +1,13 @@
 class Snek {
-    constructor(x, y) {
-      this.pos = [x, y];
-      this.color = "#00c000";
-      this.dir = 0;
-      this.len = 1;
+    constructor() {
+      this.pos = [canvas.width / 2 - (canvas.width / 2) % 40, canvas.height / 2 - (canvas.height / 2) % 40];
+      this.color = "#00a000";
+      this.bodyColor = "#00f000";
+      this.dir = -1; // Prevents movement until first input
+      this.tail = [];
     }
+
+    // Getters and setters for x/y coords
     get x() {return this.pos[0];}
     set x(newX) {this.pos[0] = newX;}
 
@@ -12,130 +15,239 @@ class Snek {
     set y(newY) {this.pos[1] = newY;}
 
     move() {
+      this.tail.push([this.x, this.y]); // Push current head pos to tail "stack"
+      /*
+      NOTE: pushing 'this.pos' rather than '[this.x, this.y]' causes the value
+      in the tail to always match 'this.pos' (snake head), triggering checkCollision()
+      and loss upon first apple collection. I don't know why using '[this.x, this.y]'
+      gets around 'this.pos' changing when they're just set from 'this.pos[0]' and 'this.pos[1]'.
+      '[this.x, this.y]' should be identical to 'this.pos' (x/y getter is pulling from pos)
+      and I think it's doing weird object stuff I don't understand :(
+      */
+
+      // Up
       if (this.dir == 0) {
         this.y -= 40;
       }
+
+      // Right
       else if (this.dir == 1) {
         this.x += 40;
       }
+
+      // Down
       else if (this.dir == 2) {
         this.y += 40;
       }
+
+      // Left
       else if (this.dir == 3) {
         this.x -= 40;
       }
+
+      // Update tail stack
+      if (appleEaten) {
+        appleEaten = false; // Don't pop if apple has been eaten, let the tail length grow by 1
+      }
+      else {
+        this.tail.shift(); // No apple, pop oldest pos off tail stack
+      }
     }
 
-    checkBounds() {
+    checkCollision() {
+      // Self collision
+      for (var i in this.tail) {
+        if (JSON.stringify(this.tail[i]) == JSON.stringify(this.pos)) {
+          console.log("Self collision at %s with tail:", this.pos);
+          console.log(snek.tail);
+          reset();
+          break; // NOTE: Will crash due to reset() of snek.tail if allowed to loop again at this point
+        }
+      }
+
+      // Right collision
       if (this.x >= canvas.width) {
-        this.pos = [160, 160];
-        this.dir = 0;
-        console.log("X bound + (right) collision")
-        snek.len = 1;
-        pause();
+        console.log("X bound + (right) collision");
+        reset();
       }
+
+      // Left collision
       else if (this.x < 0) {
-        this.pos = [160, 160];
-        this.dir = 0;
-        console.log("X bound - (left) collision")
-        pause();
-        snek.len = 1;
+        console.log("X bound - (left) collision");
+        reset();
       }
-      else if (this.y >= canvas.height) {
-        this.pos = [160, 160];
-        this.dir = 0;
-        console.log("Y bound + (down) collision")
-        pause();
-        snek.len = 1;
-      }
+
+      // Up collision
       else if (this.y < 0) {
-        this.pos = [160, 160];
-        this.dir = 0;
-        console.log("Y bound - (up) collision")
-        pause();
-        snek.len = 1;
+        console.log("Y bound - (up) collision");
+        reset();
+      }
+
+      // Down collision
+      else if (this.y >= canvas.height) {
+        console.log("Y bound + (down) collision");
+        reset();
       }
     }
 }
 
 class Apple {
   constructor() {
-    this.pos = [-40, -40];
-    this.color = "#ff0000";
+    this.pos = snek.pos;
+    this.color = "#FF0000";
   }
 
+  // Getters and setters for x/y coords
   get x() {return this.pos[0];}
   set x(newX) {this.pos[0] = newX;}
 
   get y() {return this.pos[1];}
   set y(newY) {this.pos[1] = newY;}
 
-  checkCollision() {
-    if (snek.x == this.x && snek.y == this.y) {
-      snek.len += 1;
-      console.log("Apple collected at %s len: %d", this.pos, snek.len);
+  checkEaten() {
+    // Is snek head on apple?
+    if (JSON.stringify(snek.pos) == JSON.stringify(this.pos)) {
+      appleEaten = true;
+      console.log("Apple eaten at %s / new len: %d", this.pos, snek.tail.length + 2);
       this.spawn();
     }
   }
 
   spawn() {
-    this.x = Math.floor(Math.random() * (canvas.width / 40)) * 40;
-    this.y = Math.floor(Math.random() * (canvas.height / 40)) * 40;
-    console.log("Apple spawned at " + this.pos);
+    // Ensure new apple coords are outside of snek head/tail
+    while (snek.tail.includes(JSON.stringify(this.pos)) ||
+     JSON.stringify(this.pos) == JSON.stringify(snek.pos)) {
+
+      // Generates random multiple of 40 between 0 and canvas max dimensions
+      this.pos = [Math.floor(Math.random() * (canvas.width / 40)) * 40,
+         Math.floor(Math.random() * (canvas.height / 40)) * 40];
+    }
+
+    console.log("New apple spawned at " + this.pos);
   }
+}
+
+function reset() {
+  /*
+    Purpose: Resets attribues and pauses to reset the game without needing to reload page
+    Inputs: None
+    Returns: None
+  */
+
+  console.log("Resetting game");
+  if (playing) {
+    pause();
+  }
+  snek.pos = [canvas.width / 2 - (canvas.width / 2) % 40, canvas.height / 2 - (canvas.height / 2) % 40];
+  snek.dir = -1; // Prevents movement until input
+  snek.tail = [];
+  console.log("New snek spawned at " + snek.pos);
+  apple.spawn();
 }
 
 function pause() {
-  if (playing % 2 == 1) {
-    ctx.font = "30px Arial";
-    ctx.strokeText("Paused", canvas.width / 2, canvas.height / 2);
+  /*
+    Purpose: Implements play/pause logic by setting boolean flag turning on/off frame drawing
+    Inputs: None
+    Returns: None
+  */
+
+  if (playing) {
+    // Draw pause banner
+    ctx.fillStyle = "black";
+    ctx.font = "40px Cursive";
+    ctx.fillText("PAUSED", canvas.width / 2 - 40, canvas.height / 2 - 40);
+    ctx.stroke();
+
     console.log("Execution paused");
+    playing = false;
   }
   else {
     console.log("Execution unpaused");
+    playing = true;
   }
-  playing += 1;
 }
 
 function drawAll() {
-
-  //frames += 1;
-  if (frames % 200 == 0) {
-    now = new Date();
-    msecs = now.getTime() - start.getTime();
-    console.log("fps:", (frames / msecs) * 1000);
-  }
+  /*
+    Purpose: Clears screen and draws all of the game objects for requestAnimationFrame
+    Inputs: None
+    Returns: None
+  */
 
   // Clear screen for new frame
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  // Draw background
+  ctx.beginPath();
+  ctx.fillStyle = "#c0e0f7";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
   // Draw apple
+  ctx.beginPath();
+  ctx.fillStyle = "#000000";
+  ctx.fillRect(apple.x - 1, apple.y - 1, 42, 42);
+  ctx.stroke();
+
+  ctx.beginPath();
   ctx.fillStyle = apple.color;
   ctx.fillRect(apple.x, apple.y, 40, 40);
   ctx.stroke();
 
-  // Draw snek
+  ctx.beginPath();
+  ctx.fillStyle = "#000000";
+  ctx.fillRect(apple.x + 18, apple.y + 18, 4, 4);
+  ctx.fill();
+
+  // Draw snek head
+  ctx.beginPath();
+  ctx.fillStyle = "#000000";
+  ctx.fillRect(snek.x - 1, snek.y - 1, 42, 42);
+  ctx.stroke();
+
+  ctx.beginPath();
   ctx.fillStyle = snek.color;
   ctx.fillRect(snek.x, snek.y, 40, 40);
   ctx.stroke();
 
-  // Draw score box
-  ctx.strokeStyle = snek.color;
-  ctx.rect(0, 0, 110, 30);
+  // Draw snek body
+  for (var i in snek.tail) {
+    ctx.beginPath();
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(snek.tail[i][0] - 1, snek.tail[i][1] - 1, 42, 42);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.fillStyle = snek.bodyColor;
+    ctx.fillRect(snek.tail[i][0], snek.tail[i][1], 40, 40);
+    ctx.stroke();
+  }
+
+  //Draw scores
+  ctx.beginPath();
+  ctx.fillStyle = "#000000";
+  ctx.font = "20px Cursive";
+  ctx.fillText("Score: " + (snek.tail.length + 1), 5, 25);
   ctx.stroke();
 
-  //Draw score
+  ctx.beginPath();
+  ctx.fillStyle = "#000000";
   ctx.font = "20px Cursive";
-  ctx.fillText("Score: " + snek.len, 5, 20);
+  ctx.fillText("Best: " + highScore, 5, 50);
   ctx.stroke();
 }
 
 function setUpContext() {
+  /*
+    Purpose: Sets up the canvas parameters for snake
+    Inputs: None
+    Returns: Context with set parameters
+  */
   canvas = document.getElementById("mainCanvas");
+
+  // Canvas size has to be a multiple of 40 for clean movement and spawning
   canvas.width = window.innerWidth - (window.innerWidth % 40);
   canvas.height = window.innerHeight - (window.innerHeight % 40);
-  console.log("Window is %d by %d", window.innerWidth, window.innerHeight);
-  console.log("Canvas is %d by %d", canvas.width, canvas.height);
 
   canvas.style.border = "1px solid black";
 
@@ -144,21 +256,46 @@ function setUpContext() {
 }
 
 function keypress(event) {
-  keyStr = event.key;
-  if ((playing % 2 == 0 && ["w", "a", "s", "d"].includes(keyStr))|| keyStr == 'Escape') {
+  /*
+    Purpose: Listens for keyboard events for snek movement and game controls
+    Inputs: Keyboard event
+    Returns: None
+  */
+  keyStr = event.key.toLowerCase();  // In case of caps lock
+
+  // Pause/unpause
+  if ((playing == false && ["w", "a", "s", "d"].includes(keyStr)) || keyStr == 'escape') {
     console.log("Manual pause triggered");
     pause();
   }
-  if (keyStr == 'w') {
+
+  // Reset game
+  if (keyStr == 'k') {
+    console.log("Manual reset triggered");
+    reset();
+  }
+
+  /*
+  NOTE: 180 degree turns are prevented, but still possible if keys are hit fast
+  enough to perform two 90 degree turns in the frames between snek.move() calls.
+  */
+  // Up
+  if (keyStr == 'w' && snek.dir != 2) {
     snek.dir = 0;
   }
-  if (keyStr == 'd') {
+
+  // Right
+  else if (keyStr == 'd' && snek.dir != 3) {
     snek.dir = 1;
   }
-  if (keyStr == 's') {
+
+  // Down
+  else if (keyStr == 's' && snek.dir != 0) {
     snek.dir = 2;
   }
-  if (keyStr == 'a') {
+
+  // Left
+  else if (keyStr == 'a' && snek.dir != 1) {
     snek.dir = 3;
   }
 }
